0e236ac8b852efd6c887ee160917f996d77dc6fa
Sparsh Gupta <47032539+sgupta-1@users.noreply.github.com>
2022-08-07 00:47:02 +0530

    Update Navbar.js

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import {NavLink} from "react-router-dom";
-
+import logo from '../img/logo.png';
 
 export default function Navbar() {
      
@@ -17,18 +17,18 @@ export default function Navbar() {
                               <NavLink to={'/'} className="" aria-current="page">Home</NavLink>
                             </li>
                             <li className=''>
-                              <NavLink></NavLink>
+                              <NavLink to={'/about-us'} className="">About Us</NavLink>
                             </li>
                             <li className=''>
-                              <NavLink></NavLink>
+                              <NavLink to={'/products'} className="">Products</NavLink>
                             </li>
                             <li className=''>
-                              <NavLink></NavLink>
+                              <NavLink to={'/contact-us'} className="">Contact Us</NavLink>
                             </li>
                           </ul>
                           <form className=''>
-                            <input className=''/>
-                            <button className=''></button>
+                            <input className='' placeholder='Search...'/>
+                            <button className=''>Search</button>
                           </form>
                         </div>
                       </div>
