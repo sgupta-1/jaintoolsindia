0f86cc133f9efdd2a120cbda175010138b1ca9fa
Sparsh Gupta <47032539+sgupta-1@users.noreply.github.com>
2022-08-07 21:37:56 +0530

    update

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,16 +1,34 @@
 import React from 'react'
 import {NavLink} from "react-router-dom";
+import img from '../img/logo.png'
 
 export default function Footer() {
   return (
-    <div>
-    <footer class="footer-area">        
-        <div class="copywrite-content">
-          <p class="copywrite-text">
-            <NavLink to={'/'}>
-            Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved | CO-United</NavLink></p>
-          </div>
+    <div class="container">
+  <footer class="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
+    <p class="col-md-4 mb-0 text-muted">&copy; 2022 Jain Tools India</p>
+
+    <NavLink to={"/"} class="col-md-4 d-flex align-items-center justify-content-center mb-3 mb-md-0 me-md-auto link-dark text-decoration-none">
+      <img src={img} className='img1' alt=''/>
+    </NavLink>
+
+    <ul class="nav col-md-4 justify-content-end footer">
+      <li class="nav-item"><NavLink to={"/"} className='text-decoration-none px-2 text1'>Home</NavLink></li>
+      <li class="nav-item"><NavLink to={"/about-us"} className='text-decoration-none px-2 text1'>About Us</NavLink></li>
+      <li class="nav-item"><NavLink to={"/products"} className='text-decoration-none px-2 text1'>Products</NavLink></li>
+      <li class="nav-item"><NavLink to={"/contact-us"} className='text-decoration-none px-2 text1'>Contact Us</NavLink></li>
+    </ul>
     </footer>
+    <style>{
+    `
+    .text1{
+      color: #6c757d;
+    }
+    .img1{
+      height: 32px
+    }
+    `
+    }</style>
     </div>
   );
 }
--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -13,7 +13,7 @@ export default function Navbar() {
               <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
               </button>
-              <div className="collapse navbar-collapse" id="navbarSupportedContent">
+              <div className="collapse navbar-collapse " id="navbarSupportedContent">
                 <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                   <li className="nav-item">
                     <NavLink className="nav-link" aria-current="page" to={'/'} style={({ isActive }) => ({color: isActive ? '#14927b' : '#000'})}>Home</NavLink>
