8b1686b35a470d39fed43e4a6d5f3a60dc884f01
Sparsh Gupta <47032539+sgupta-1@users.noreply.github.com>
2022-08-08 22:47:52 +0530

    update

--- src/Components/About.js
+++ src/Components/About.js
@@ -2,6 +2,7 @@ import React from 'react'
 import Navbar from './Navbar'
 import Footer from './Footer'
 import $ from 'jquery'
+import logo from '../img/logo.png'
 
 
 $(window).scroll(function() {
@@ -21,26 +22,26 @@ export default function About() {
     <div>
       <Navbar/>
 <div className='aboutus'>      
-<div id="about" class="container-fluid">
-  <div class="row">
-    <div class="col-sm-8">
-      <h2>About Company Page</h2><br/>
+<div id="about" className="container-fluid">
+  <div className="row">
+    <div className="col-sm-8">
+      <h2>About Our Company</h2><br/>
       <h4>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</h4><br/>
       <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-      <br/><button class="btn btn-default btn-lg">Get in Touch</button>
+      <br/><button className="btn btn-default btn-lg">Get in Touch</button>
     </div>
-    <div class="col-sm-4">
-      <span class="glyphicon glyphicon-signal logo"></span>
+    <div className="col-sm-4">
+      <img className='logo' src={logo} alt='JTI logo'/>
     </div>
   </div>
 </div>
 
-<div class="container-fluid bg-grey">
-  <div class="row">
-    <div class="col-sm-4">
-      <span class="glyphicon glyphicon-globe logo slideanim"></span>
+<div className="container-fluid bg-grey">
+  <div className="row">
+    <div className="col-sm-4">
+      <span className="fa-solid fa-globe fa-10x slideanim"></span>
     </div>
-    <div class="col-sm-8">
+    <div className="col-sm-8">
       <h2>Our Values</h2><br/>
       <h4><strong>MISSION:</strong> Our mission lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</h4><br/>
       <p><strong>VISION:</strong> Our vision Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
@@ -49,67 +50,67 @@ export default function About() {
   </div>
 </div>
 
-<div id="services" class="container-fluid text-center">
+<div id="services" className="container-fluid text-center">
   <h2>SERVICES</h2>
   <h4>What we offer</h4>
   <br/>
-  <div class="row slideanim">
-    <div class="col-sm-4">
-      <span class="glyphicon glyphicon-off logo-small"></span>
+  <div className="row slideanim">
+    <div className="col-sm-4">
+      <span className="glyphicon glyphicon-off logo-small"></span>
       <h4>POWER</h4>
       <p>Lorem ipsum dolor sit amet..</p>
     </div>
-    <div class="col-sm-4">
-      <span class="glyphicon glyphicon-heart logo-small"></span>
+    <div className="col-sm-4">
+      <span className="glyphicon glyphicon-heart logo-small"></span>
       <h4>LOVE</h4>
       <p>Lorem ipsum dolor sit amet..</p>
     </div>
-    <div class="col-sm-4">
-      <span class="glyphicon glyphicon-lock logo-small"></span>
+    <div className="col-sm-4">
+      <span className="glyphicon glyphicon-lock logo-small"></span>
       <h4>JOB DONE</h4>
       <p>Lorem ipsum dolor sit amet..</p>
     </div>
   </div>
   <br/><br/>
-  <div class="row slideanim">
-    <div class="col-sm-4">
-      <span class="glyphicon glyphicon-leaf logo-small"></span>
+  <div className="row slideanim">
+    <div className="col-sm-4">
+      <span className="glyphicon glyphicon-leaf logo-small"></span>
       <h4>GREEN</h4>
       <p>Lorem ipsum dolor sit amet..</p>
     </div>
-    <div class="col-sm-4">
-      <span class="glyphicon glyphicon-certificate logo-small"></span>
+    <div className="col-sm-4">
+      <span className="glyphicon glyphicon-certificate logo-small"></span>
       <h4>CERTIFIED</h4>
       <p>Lorem ipsum dolor sit amet..</p>
     </div>
-    <div class="col-sm-4">
-      <span class="glyphicon glyphicon-wrench logo-small"></span>
+    <div className="col-sm-4">
+      <span className="glyphicon glyphicon-wrench logo-small"></span>
       <h4>HARD WORK</h4>
       <p>Lorem ipsum dolor sit amet..</p>
     </div>
   </div>
 </div>
 
-<div id="portfolio" class="container-fluid text-center bg-grey">
+<div id="portfolio" className="container-fluid text-center bg-grey">
   <h2>Portfolio</h2><br/>
   <h4>What we have created</h4>
-  <div class="row text-center slideanim">
-    <div class="col-sm-4">
-      <div class="thumbnail">
+  <div className="row text-center slideanim">
+    <div className="col-sm-4">
+      <div className="thumbnail">
         <img src="paris.jpg" alt="Paris" width="400" height="300"/>
         <p><strong>Paris</strong></p>
         <p>Yes, we built Paris</p>
       </div>
     </div>
-    <div class="col-sm-4">
-      <div class="thumbnail">
+    <div className="col-sm-4">
+      <div className="thumbnail">
         <img src="newyork.jpg" alt="New York" width="400" height="300"/>
         <p><strong>New York</strong></p>
         <p>We built New York</p>
       </div>
     </div>
-    <div class="col-sm-4">
-      <div class="thumbnail">
+    <div className="col-sm-4">
+      <div className="thumbnail">
         <img src="sanfran.jpg" alt="San Francisco" width="400" height="300"/>
         <p><strong>San Francisco</strong></p>
         <p>Yes, San Fran is ours</p>
