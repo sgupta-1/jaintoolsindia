f930d854accd63bd32f0f56aaa7997c386aa03cb
VScode2000 <84017107+VScode2000@users.noreply.github.com>
2022-08-08 14:26:51 +0530

    dont know what it is

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,46 +1,97 @@
-import React from 'react'
-import Navbar from './Navbar'
-import Footer from './Footer'
-import img1 from '../img/carousel/1.jpg';
-import img2 from '../img/carousel/2.jpg';
-import img3 from '../img/carousel/3.jpg';
-import img4 from '../img/carousel/6.jpg';
-import img5 from '../img/carousel/5.jpg';
+import React from "react";
+import Navbar from "./Navbar";
+import Footer from "./Footer";
+import img1 from "../img/carousel/1.jpg";
+import img2 from "../img/carousel/2.jpg";
+import img3 from "../img/carousel/3.jpg";
+import img4 from "../img/carousel/6.jpg";
+import img5 from "../img/carousel/5.jpg";
 
 export default function Home() {
   return (
     <div>
-        <Navbar/>
-          
-        <div id="carouselDark" className="carousel carousel-light carousel-fade slide" data-bs-ride="carousel">
-  <div className="carousel-indicators">
-    <button type="button" data-bs-target="#carouselDark" data-bs-slide-to="0" className="active" aria-current="true" aria-label="Slide 1"></button>
-    <button type="button" data-bs-target="#carouselDark" data-bs-slide-to="1" aria-label="Slide 2"></button>
-    <button type="button" data-bs-target="#carouselDark" data-bs-slide-to="2" aria-label="Slide 3"></button>
-    <button type="button" data-bs-target="#carouselDark" data-bs-slide-to="3" aria-label="Slide 4"></button>
-    <button type="button" data-bs-target="#carouselDark" data-bs-slide-to="4" aria-label="Slide 5"></button>
-  </div>
+      <Navbar />
 
-  <div className="carousel-inner">
-    <div className="carousel-item active" data-bs-interval="2000" data-touch='true'>
-      <img src={img1} className="d-block w-100" alt="..."/>
-    </div>
-    <div className="carousel-item" data-bs-interval="2000" data-touch='true'>
-      <img src={img2} className="d-block w-100" alt="..."/>
-    </div>
-    <div className="carousel-item" data-touch='true' data-bs-interval="2000">
-      <img src={img3} className="d-block w-100" alt="..."/>
-    </div>
-    <div className="carousel-item" data-touch='true' data-bs-interval="2000">
-      <img src={img4} className="d-block w-100" alt="..."/>
-    </div>
-    <div className="carousel-item" data-touch='true' data-bs-interval="2000">
-      <img src={img5} className="d-block w-100" alt="..."/>
-    </div>
-  </div>
-</div>
+      <div
+        id="carouselDark"
+        className="carousel carousel-light carousel-fade slide"
+        data-bs-ride="carousel"
+      >
+        <div className="carousel-indicators">
+          <button
+            type="button"
+            data-bs-target="#carouselDark"
+            data-bs-slide-to="0"
+            className="active"
+            aria-current="true"
+            aria-label="Slide 1"
+          ></button>
+          <button
+            type="button"
+            data-bs-target="#carouselDark"
+            data-bs-slide-to="1"
+            aria-label="Slide 2"
+          ></button>
+          <button
+            type="button"
+            data-bs-target="#carouselDark"
+            data-bs-slide-to="2"
+            aria-label="Slide 3"
+          ></button>
+          <button
+            type="button"
+            data-bs-target="#carouselDark"
+            data-bs-slide-to="3"
+            aria-label="Slide 4"
+          ></button>
+          <button
+            type="button"
+            data-bs-target="#carouselDark"
+            data-bs-slide-to="4"
+            aria-label="Slide 5"
+          ></button>
+        </div>
 
-        <Footer/>
+        <div className="carousel-inner">
+          <div
+            className="carousel-item active"
+            data-bs-interval="2000"
+            data-touch="true"
+          >
+            <img src={img1} className="d-block w-100 " alt="..." />
+          </div>
+          <div
+            className="carousel-item"
+            data-bs-interval="2000"
+            data-touch="true"
+          >
+            <img src={img2} className="d-block w-100 " alt="..." />
+          </div>
+          <div
+            className="carousel-item"
+            data-touch="true"
+            data-bs-interval="2000"
+          >
+            <img src={img3} className="d-block w-100 " alt="..." />
+          </div>
+          <div
+            className="carousel-item"
+            data-touch="true"
+            data-bs-interval="2000"
+          >
+            <img src={img4} className="d-block w-100 " alt="..." />
+          </div>
+          <div
+            className="carousel-item"
+            data-touch="true"
+            data-bs-interval="2000"
+          >
+            <img src={img5} className="d-block w-100 " alt="..." />
+          </div>
+        </div>
+      </div>
+
+      <Footer />
     </div>
-  )
+  );
 }
